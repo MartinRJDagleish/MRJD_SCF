d288db43dceb744ff9888a1e41cd80ba766e54c7
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2022-11-04 21:45:59 +0100

    Water with Molecule class in separate file

--- water.cc
+++ water.cc
@@ -8,5 +8,35 @@ int main(int argc, char *argv[]){
     h2o.num_atoms = 3;
     h2o.charge = 0;
     h2o.Z_vals = new int[h2o.num_atoms]; // defines array of atomic numbers
-    
+    h2o.geom = new double*[h2o.num_atoms]; // defines array of atomic coordinates
+
+    for (int i = 0; i < h2o.num_atoms; i++){
+        h2o.geom[i] = new double[3];
+    }
+
+    h2o.Z_vals[0] = 8;
+    h2o.geom[0][0] =  0.000000000000;
+    h2o.geom[0][1] =  0.000000000000;
+    h2o.geom[0][2] = -0.122368916506;
+    h2o.Z_vals[1] = 1;
+    h2o.geom[1][0] =  0.000000000000;
+    h2o.geom[1][1] =  1.414995841403;
+    h2o.geom[1][2] =  0.971041753535;
+    h2o.Z_vals[2] = 1;
+    h2o.geom[2][0] =  0.000000000000;
+    h2o.geom[2][1] = -1.414995841403;
+    h2o.geom[2][2] =  0.971041753535;
+
+    h2o.print_geom();
+    cout << "\n" << endl;
+    h2o.translate(1.0, 1.0, 1.0);
+    h2o.print_geom();
+
+    delete [] h2o.Z_vals;
+    for (int i = 0; i < h2o.num_atoms; i++){
+        delete [] h2o.geom[i];
+    }
+    delete [] h2o.geom;
+
+    return 0;
 }
