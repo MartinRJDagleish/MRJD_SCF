472ae58be48cb51a89d8b3cb7b5499bd25cde51f
MartinRJDagleish <martin.dagleish@stud.uni-regensburg.de>
2022-11-04 20:41:31 +0100

    Cmake template

---
+++ build/CMakeFiles/MRJD_SCF.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/run/media/mrjd/DATA/Programming/C++/Crawford_Grp_MRJD/main.cpp" "CMakeFiles/MRJD_SCF.dir/main.cpp.o" "gcc" "CMakeFiles/MRJD_SCF.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/MRJD_SCF.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MRJD_SCF.dir/main.cpp.o"
+  "CMakeFiles/MRJD_SCF.dir/main.cpp.o.d"
+  "MRJD_SCF"
+  "MRJD_SCF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MRJD_SCF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
